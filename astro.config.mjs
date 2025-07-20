f0d8da5c7e96adca734aaf9e67599888c4979fd2
David3022 <davidafs36@gmail.com>
2025-07-20 11:08:00 -0600

    starting the migration from previous project to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 // @ts-check
+// import { defineConfig } from 'astro/config';
+// import mdx from '@astrojs/mdx';
+// import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+// export default defineConfig({
+// 	site: 'https://example.com',
+// 	integrations: [mdx(), sitemap()],
+// });
+
+
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind'; // 👈 agrega esta línea
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), tailwind()], // 👈 agrégalo aquí también
 });
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
